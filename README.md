8a635496668809b7ca2ebd97c650aa3f29d8d302
BeiyanLuansheng <941197279@qq.com>
2020-11-23 11:02:03 +0800

    framework

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # GWARBMS
-General wholesale and retail business management system 通用批发零售业务管理系统
+General wholesale and retail business management system 
+
+通用批发零售业务管理系统
+
+基于Spring Boot开发，前端使用LayUI框架
---
+++ src/main/java/cn/edu/hit/spat/GWARBMSApp.java
@@ -0,0 +1,24 @@
+package cn.edu.hit.spat;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author XuJian
+ */
+@EnableAsync
+@SpringBootApplication
+@EnableTransactionManagement
+@MapperScan("cn.hit.edu.spat.*.mapper")
+public class GWARBMSApp {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(GWARBMSApp.class)
+                .web(WebApplicationType.SERVLET)
+                .run(args);
+    }
+}
