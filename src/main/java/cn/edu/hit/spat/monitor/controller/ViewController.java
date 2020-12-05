62f8783f233a4f62c7caa00b765b396f2c95cf3c
BeiyanLuansheng <941197279@qq.com>
2020-12-05 13:28:18 +0800

    删除系统监控菜单，仅保留查看在线用户功能

--- src/main/java/cn/edu/hit/spat/monitor/controller/ViewController.java
+++ src/main/java/cn/edu/hit/spat/monitor/controller/ViewController.java
@@ -2,20 +2,12 @@ package cn.edu.hit.spat.monitor.controller;
 
 import cn.edu.hit.spat.common.entity.GwarbmsConstant;
 import cn.edu.hit.spat.common.utils.GwarbmsUtil;
-import cn.edu.hit.spat.monitor.entity.JvmInfo;
-import cn.edu.hit.spat.monitor.entity.ServerInfo;
-import cn.edu.hit.spat.monitor.entity.TomcatInfo;
-import cn.edu.hit.spat.monitor.helper.GwarbmsActuatorHelper;
 import lombok.RequiredArgsConstructor;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
-import static cn.edu.hit.spat.monitor.endpoint.GwarbmsMetricsEndpoint.GwarbmsMetricResponse;
 
 /**
  * @author MrBird
@@ -25,64 +17,10 @@ import static cn.edu.hit.spat.monitor.endpoint.GwarbmsMetricsEndpoint.GwarbmsMet
 @RequiredArgsConstructor
 public class ViewController {
 
-    private final GwarbmsActuatorHelper actuatorHelper;
-
     @GetMapping("online")
     @RequiresPermissions("online:view")
     public String online() {
         return GwarbmsUtil.view("monitor/online");
     }
 
-    @GetMapping("log")
-    @RequiresPermissions("log:view")
-    public String log() {
-        return GwarbmsUtil.view("monitor/log");
-    }
-
-    @GetMapping("loginlog")
-    @RequiresPermissions("loginlog:view")
-    public String loginLog() {
-        return GwarbmsUtil.view("monitor/loginLog");
-    }
-
-    @GetMapping("httptrace")
-    @RequiresPermissions("httptrace:view")
-    public String httptrace() {
-        return GwarbmsUtil.view("monitor/httpTrace");
-    }
-
-    @GetMapping("jvm")
-    @RequiresPermissions("jvm:view")
-    public String jvmInfo(Model model) {
-        List<GwarbmsMetricResponse> jvm = actuatorHelper.getMetricResponseByType("jvm");
-        JvmInfo jvmInfo = actuatorHelper.getJvmInfoFromMetricData(jvm);
-        model.addAttribute("jvm", jvmInfo);
-        return GwarbmsUtil.view("monitor/jvmInfo");
-    }
-
-    @GetMapping("tomcat")
-    @RequiresPermissions("tomcat:view")
-    public String tomcatInfo(Model model) {
-        List<GwarbmsMetricResponse> tomcat = actuatorHelper.getMetricResponseByType("tomcat");
-        TomcatInfo tomcatInfo = actuatorHelper.getTomcatInfoFromMetricData(tomcat);
-        model.addAttribute("tomcat", tomcatInfo);
-        return GwarbmsUtil.view("monitor/tomcatInfo");
-    }
-
-    @GetMapping("server")
-    @RequiresPermissions("server:view")
-    public String serverInfo(Model model) {
-        List<GwarbmsMetricResponse> jdbcInfo = actuatorHelper.getMetricResponseByType("jdbc");
-        List<GwarbmsMetricResponse> systemInfo = actuatorHelper.getMetricResponseByType("system");
-        List<GwarbmsMetricResponse> processInfo = actuatorHelper.getMetricResponseByType("process");
-
-        ServerInfo serverInfo = actuatorHelper.getServerInfoFromMetricData(jdbcInfo, systemInfo, processInfo);
-        model.addAttribute("server", serverInfo);
-        return GwarbmsUtil.view("monitor/serverInfo");
-    }
-
-    @GetMapping("swagger")
-    public String swagger() {
-        return GwarbmsUtil.view("monitor/swagger");
-    }
 }
